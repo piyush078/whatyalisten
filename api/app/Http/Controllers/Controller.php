deb587e986ca4a7e52e71f7ced0a4ca915c75822
Piyush Madhusudan <piyushdevil75@gmail.com>
2018-01-11 00:26:10 +0530

    Adjust reuse of the api token till timeout

--- api/app/Http/Controllers/Controller.php
+++ api/app/Http/Controllers/Controller.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Config;
+use \DateTime;
+use \Session;
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -13,13 +16,112 @@ class Controller extends BaseController
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
     /**
+     * GuzzleHttp variable.
+     *
+     * @var \GuzzleHttp\Guzzle\Client
+     */
+    public $client = null;
+
+    /**
+     * Session token variable.
+     *
+     * @var string
+     */
+    private $sessionTokenName = 'apiToken';
+
+    /**
+     * Constructor of Controller.
+     *
+     * @return void
+     */
+    public function __construct ()
+    {
+        $this->client = new \GuzzleHttp\Client ();
+    }
+
+    /**
      * Return Spotify headers.
      *
-     * @return array
+     * @param  void
+     * @return string
      */
-    public function getSpotifyHeaders () 
+    public function getAuthorizationCode () 
     {
     	$headers = Config::get ('client');
-    	return $headers;
+        $client_id = $headers ['client_id'];
+        $client_secret = $headers ['client_secret'];
+        return base64_encode ($client_id.':'.$client_secret);
+    }
+
+    /**
+     * Check if token validity is over.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return boolean
+     */
+    private function isValidToken (Request $request)
+    {
+        $now = new DateTime ();
+        $newTimestamp = $now->getTimestamp ();
+        $oldTimestamp = Session::get ($this->sessionTokenName) ['timestamp'];
+        return ($newTimestamp - $oldTimestamp <= 3600);
+    }
+
+    /**
+     * Refresh the Spotify token and store the new values in session.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return boolean|mixed
+     */
+    private function refreshToken (Request $request)
+    {
+        try {
+            $response = $this->client->request ('POST', 'https://accounts.spotify.com/api/token', [
+                'headers' => [
+                    'Accept' => 'application/json',
+                    'Authorization' => 'Basic '.$this->getAuthorizationCode (),
+                ],
+                'form_params' => [
+                    'grant_type' => 'client_credentials'
+                ]
+            ]);
+        } catch (GuzzleHttp\Exception\BadResponseException $error) {
+            return false;
+        }
+        $contents = json_decode ($response->getBody ()->getContents ());
+        Session::put ($this->sessionTokenName, [
+            'timestamp' => (new DateTime ())->getTimestamp (),
+            'tokenObject' => $contents
+        ]);
+        Session::save ();
+        return $contents;
+    }
+
+    /**
+     * Initialize the Spotify token.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return boolean|mixed
+     */
+    private function initializeToken (Request $request)
+    {
+        return $this->refreshToken ($request);
+    }
+
+    /** 
+     * Return the valid Spotify token.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return mixed
+     */
+    public function getSpotifyToken (Request $request)
+    {
+        if (! Session::has ($this->sessionTokenName) || ! array_key_exists ('timestamp', Session::get ($this->sessionTokenName))) {
+            return $this->initializeToken ($request);
+        } else if (! $this->isValidToken ($request)) {
+            return $this->refreshToken ($request);
+        } else {
+            return Session::get ($this->sessionTokenName) ['tokenObject'];
+        }
     }
 }
--- api/app/Http/Controllers/SearchController.php
+++ api/app/Http/Controllers/SearchController.php
@@ -2,49 +2,36 @@
 
 namespace App\Http\Controllers;
 
-use Config;
+use \Exception;
+use \Response;
 use GuzzleHttp;
 use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
-	/**
-	 * Search for a songs title.
+    /**
+	 * Search for a artist name.
 	 *
 	 * @param  \Illuminate\Http\Request  $request
 	 * @return \GuzzleHttp\Guzzle\Client
 	 */
-    public function artists (Request $request, $title, $limit) {
-
-        $headers = $this->getSpotifyHeaders ();
-        $client_id = $headers ['client_id'];
-        $client_secret = $headers ['client_secret'];
-        $client = new GuzzleHttp\Client ();
-
-        $authorization = base64_encode ($client_id.':'.$client_secret);
-
+    public function artists (Request $request, $title, $limit) 
+    {
         try {
-        	$tokenResponse = $client->request ('POST', 'https://accounts.spotify.com/api/token', [
-        		'headers' => [
-        			'Accept' => 'application/json',
-        			'Authorization' => 'Basic '.$authorization,
-        		],
-        		'form_params' => [
-        			'grant_type' => 'client_credentials'
-        		]
-        	]);
-
-        	$contents = json_decode ($tokenResponse->getBody ()->getContents ());
-
+            $contents = $this->getSpotifyToken ($request);
+            if (! $contents) {
+                dd ('Cannot complete the request.');
+                return;
+            }
             $access_token = $contents->access_token;
-        	$token_type = $contents->token_type;
+            $token_type = $contents->token_type;
 
         	$url = 'https://api.spotify.com/v1/search';
         	$query = $title;
         	$type = 'artist';
             $limit = ($limit && (int)$limit <= 5 && (int)$limit > 0) ? $limit : 5;
 
-        	$response = $client->request ('GET', $url, [
+        	$response = $this->client->request ('GET', $url, [
         		'headers' => [
         			'Accept' => 'application/json',
         			'Authorization' => $token_type.' '.$access_token,
@@ -55,13 +42,11 @@ class SearchController extends Controller
                     'limit' => $limit
         		]
         	]);
-
-        	$response = json_decode ($response->getBody ()->getContents ());
-        	dd ($response);
+        	$response = '('.$response->getBody ()->getContents ().')';
+        	return Response ($response, 200);
 
         } catch (GuzzleHttp\Exception\BadResponseException $error) {
-        	dd ($error);
+        	dd ('Something wrong.');
         }
-
     }
 }
